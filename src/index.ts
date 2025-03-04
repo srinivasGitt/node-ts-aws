2628df813bed08737f478935c484d456c00c8cb3
Srinivas <srinivasgollapalli91@gmail.com>
2025-03-04 15:48:36 +0530

    small update

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(userRoutes);
 
+app.get('/', (req, res) => {
+    res.send('hi hw r i')
+})
+
 // Global error handler (optional but recommended)
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.error(err);
