c7de1dd318a9a7308e3bd12ff0f9372b6e349c4b
Srinivas <srinivasgollapalli91@gmail.com>
2025-03-04 15:31:47 +0530

    port changed

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import userRoutes from './routes/userRoutes';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(userRoutes);
