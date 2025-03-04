3ca15e16f2d842edcfec4e50a04a9a68891f6ddf
Srinivas <srinivasgollapalli91@gmail.com>
2025-03-04 16:12:35 +0530

    build command updated

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.use(express_1.default.json());
 app.use(userRoutes_1.default);
+app.get('/', (req, res) => {
+    res.send('hi hw r i');
+});
 // Global error handler (optional but recommended)
 app.use((err, req, res, next) => {
     console.error(err);
